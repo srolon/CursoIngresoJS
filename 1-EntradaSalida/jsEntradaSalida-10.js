48544b13f5639cbddf960a3ad55cfa52c38c77b6
alumno <alumno@utnfra.com.ar>
2020-02-06 22:12:02 -0300

    ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+     	var numero1;
+        var resultado;
+        var numeroparseado1;
+        var porcentaje =0.75;
+
+        numero1 = document.getElementById("importe").value;
+        numeroparseado1= parseInt(numero1);
+        resultado = numeroparseado1 * porcentaje ;
+        document.getElementById("resultado").value = resultado
+       
 	
 }
