03354ab686610c4af6dc22bf609210a74b0e8b92
srolon <60716658+srolon@users.noreply.github.com>
2020-02-19 21:39:46 -0300

    ejercicio 6 de interaciones

--- jsIteraciones (6).js
+++ jsIteraciones (6).js
@@ -1,12 +1,28 @@
-function mostrar()
-{
+function mostrar() {
 
-	var contador=0;
-	var acumulador=0;
+	var contador = 0;
+	var acumulador = 0;
+
+	while (contador < 5) {
+		contador++;
+		var numero = prompt("ingrese un numero");
+		numero = parseInt(numero);
+
+		// validar
+
+		while (isNan(numero)) {
+			numero = prompt(" ingrese un numero valido");
+			numero = parseInt(numero);
+
+		}
+
+		//acumular
+		acumulador + - numero;
+	}
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / 5;
 
 }//FIN DE LA FUNCIÓN
