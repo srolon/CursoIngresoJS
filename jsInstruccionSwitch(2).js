1cbdfeb7d98a8ba649c7c4f030631d52b73c91bf
srolon <60716658+srolon@users.noreply.github.com>
2020-02-20 13:21:10 -0300

    ejercicios 1,2,3 switch

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,36 @@
+function mostrar()
+{
+//tomo la edad  
+    var mesDelAño = document.getElementById('mes').value;
+    switch (mesdelaño) 
+    {
+        
+        case "Junio":
+              alert("falta para el invierno");
+              break;    
+        case "Julio":
+             alert("Abrigate que hace frio");
+              break;
+        case "Agosto":
+             alert("Abrigate que hace frio"); 
+        case "Septiembre":
+            alert("Ya pasamos el frio, ahora calor"); 
+        case "Octubre":
+            alert("Ya pasamos el frio, ahora calor");  
+        case "Noviembre":
+            alert("Ya pasamos el frio, ahora calor");   
+        case "Diciembre":
+            alert("Ya pasamos el frio, ahora calor"); 
+            default
+            alert("falta para el invierno");                    
+ }
+
+
+
+
+alert (mesDelAño);
+
+
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,21 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+	
+    switch (mes) {
+      
+        case "Febrero":
+        alert("Este mes no tiene mas de 29 dias");
+        break;
+        default
+        alert("Este mes tiene más de 29 dias");
+
+        
+    }
+    
+
+
+}//FIN DE LA FUNCIÓN
