7bd0ac9233933735b205e514a10a4affdb569773
srolon <60716658+srolon@users.noreply.github.com>
2020-02-19 22:01:58 -0300

    ejercicio 7 incompleto

--- jsIteraciones (7).js
+++ jsIteraciones (7).js
@@ -4,6 +4,24 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	
+
+	while (contador > 1) {
+		contador++;
+		var numero = prompt("ingrese un numero");
+		numero = parseInt(numero);
+
+		// validar
+
+		while (isNan(numero)) {
+			numero = prompt(" ingrese un numero valido");
+			numero = parseInt(numero);
+
+		}
+
+		//acumular
+		acumulador + - numero;
+	}
 
 
 document.getElementById('suma').value=acumulador;
