c59684728990e9f488fd8581a87126bd3e86faa2
srolon <60716658+srolon@users.noreply.github.com>
2020-02-20 13:17:31 -0300

    ejercicio 7 if

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,20 @@
+function mostrar()
+{
+//tomo la edad  
+
+var edad;
+var estadocivil;
+edad= document.getElementById("edad").value;
+estadocivil= document.getElementById("estadoCivil").value;
+
+
+if ( edad<18) {
+    alert("usted es muy joven para no ser soltero");
+}
+
+
+
+	
+
+
+}//FIN DE LA FUNCIÓN
