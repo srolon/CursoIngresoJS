5e22227ba657ec834c154e69643c86e2b69c7e6a
srolon <60716658+srolon@users.noreply.github.com>
2020-02-26 20:48:32 -0300

    Iteraciones For

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,12 @@
 function mostrar()
 {
+var contador;
+
+
+    for( contador = 1; contador < 11; contador++){
+        console.log(contador);
+        document.write( contador + "<br>");
+}
+
 
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,13 @@
 function mostrar()
 {
 
+    var contador;
+
+
+    for( contador = 1; contador > 11; contador++){
+        console.log(contador);
+        document.write( contador + "<br>");
+}
+
 
 }
