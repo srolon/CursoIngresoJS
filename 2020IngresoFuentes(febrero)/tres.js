288fc83ea1ca7db240a305c599653251fce81b1f
srolon <60716658+srolon@users.noreply.github.com>
2020-03-09 00:36:01 -0300

    ejercicios evaluacion 2020

--- 2020IngresoFuentes(febrero)/tres.js
+++ 2020IngresoFuentes(febrero)/tres.js
@@ -1,4 +1,99 @@
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+
+	var respuesta;
+	var nombre;
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var hombreCasadoMasJoven;
+	var nombreCasadoMasJoven
+	var pasajeroMasViejo;
+	var promedioEdad;
+	var pasajeroMasViejo;
+	var nombrePasajeroMasViejo;
+	var sexoPasajeroMasViejo;
+	var acumuladorMujeres = 0;
+	var acumuladorHombres = 0;
+	var promedioHombresSolteros;
+	var contadorHombresSolteros = 0;
+	var promedioMujeres;
+	var flag = 0;
+	var contadorMujeres = 0;
+	var contadorMujeresCasadas = 0;
+	do {
+
+
+		nombre = prompt("Ingrese nombre");
+
+		edad = parseInt(prompt("Ingrese edad"));
+
+		while (edad < 18) {
+			edad = parseInt(prompt("Ingrese edad mayor a 18 anios"));
+		}
+
+		sexo = prompt("ingrese sexo entre f o m");
+
+		while (sexo != "f" && sexo != "m") {
+			sexo = prompt("dato incorrecto, ingrese sexo entre f o m ");
+		}
+
+		estadoCivil = prompt("Ingrese estado civil (casado, viudo o soltero)")
+
+		while (estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo") {
+			estadoCivil = prompt("Dato incorrecto, Ingrese estado civil entre, soltero, casado y viudo");
+		}
+
+		if ((sexo == "m" && estadoCivil == "casado" && edad < hombreCasadoMasJoven) || flag == 0) {
+			hombreCasadoMasJoven = edad;
+			nombreCasadoMasJoven = nombre;
+
+		}
+		if (edad > pasajeroMasViejo || flag == 0) {
+			pasajeroMasViejo = edad;
+			nombrePasajeroMasViejo = nombre;
+			sexoPasajeroMasViejo = sexo;
+			flag = 1;
+		}
+
+		if (sexo == "f" && (estadoCivil == "viuda" || estadoCivil == "casada")) {
+			contadorMujeresCasadas++;
+
+		}
+		if (sexo == "f") {
+			acumuladorMujeres = acumuladorMujeres + edad;
+			contadorMujeres++;
+
+		}
+
+		if (sexo == "m" && estadoCivil == "soltero") {
+			acumuladorHombres = acumuladorHombres + edad;
+			contadorHombresSolteros++;
+
+		}
+
+
+
+
+		respuesta = prompt("Desea continuar? (s para continuar)");
+
+
+
+
+	} while (respuesta == "s");
+
+
+
+	promedioMujeres = acumuladorMujeres / contadorMujeres;
+	promedioHombresSolteros = acumuladorHombres / contadorHombresSolteros;
+
+
+	document.write("El nombre del casado más joven es : " + nombreCasadoMasJoven + "</br>");
+	document.write("El sexo del pasajero/a más viejo es  : " + sexoPasajeroMasViejo + "</br>");
+	document.write("Cantidad de mujeres casadas o viudas es : " + contador + "</br>");
+	document.write("El promedio de mujeres es : " + promedioMujeres + "</br>");
+	document.write("El promedio de edad entre los hombres solteros es : " + promedioHombresSolteros + "</br>");
+
+
+
+
 }
--- 2020IngresoFuentes(febrero)/uno.js
+++ 2020IngresoFuentes(febrero)/uno.js
@@ -1,5 +1,126 @@
-
-function mostrar()
+Function mostrar() 
 {
-	alert("uno");
+
+
+	var tipo;
+	var precio;
+	var cantidad;
+	var cantidadMaximaBarbijo;
+	var marca;
+	var fabricante;
+	var contador = 0;
+	var acumuladorJabon = 0;
+	var productoMaximo;
+	var precioBarbijoMaximo;
+	var flag = 0;
+	var fabricanteMaximoBarbijo;
+	var cantidadMaxima;
+	var fabricanteCantidadMaximo;
+
+
+
+	for (contador; contador < 5; contador++ ) {
+		tipo = prompt("Ingrese tipo de insumo entre barbijo, jabon y alcohol");
+
+		while (tipo != "jabon" && tipo != "alcohol" && tipo != "barbijo") {
+			tipo = prompt("Dato invaido, ingrese tipo de insumo entre barbijo, jabon y alcohol");
+
+		}
+		precio = parseInt(prompt("Ingrese precio entre 100 y 300"));
+		while (precio < 100 || precio > 300 || isNaN(precio)) {
+			precio = parseInt(prompt("Dato invalido, Ingrese precio entre 100 y 300"));
+
+		}
+		cantidad = parseInt(prompt("Ingrese cantidad entre 1 y 1000"));
+		while (cantidad < 1 || cantidad > 1000 || isNaN(cantidad)) {
+			cantidad = parseInt(prompt("Dato invalido, Ingrese cantidad entre 1 y 1000"));
+
+		}
+
+		marca = prompt("Ingrese marca");
+		fabricante = prompt("Ingrese fabricante");
+
+		// Punto A
+
+
+		if ((tipo == "barbijo" && precio > precioBarbijoMaximo) || flag == 0) {
+			precioBarbijoMaximo = precio;
+			cantidadMaximaBarbijo = cantidad;
+			fabricanteMaximoBarbijo = fabricante;
+
+
+		}
+		// Punto B
+
+		if (cantidad > cantidadMaxima || flag == 0)
+			cantidadMaxima = cantidad;
+		fabricanteCantidadMaximo = fabricante;
+		flag = 1;
+
+
+		// Punto C
+
+		if (tipo == "jabon") {
+			acumuladorJabon = acumuladorJabon + cantidad;
+		}
+
+
+	}
+
+
+	document.write("De los más caro de los barbijos la cantidad es : " + cantidadMaximaBarbijo +
+		" y el fabricante de barbijos más caro es : " + fabricanteMaximoBarbijo + "</br>");
+	document.write(" El fabricante con más unidades es : " + fabricanteCantidadMaximo + "</br>");
+	document.write(" Las cantidades totales de jabón son  : " + acumuladorJabon + "</br>");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 }
